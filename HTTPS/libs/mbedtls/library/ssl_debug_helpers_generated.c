839b46887e32d7b47ce0a8f985ebccf90088be96
alejandro-ayala <99070698+alejandro-ayala@users.noreply.github.com>
2022-11-26 23:34:07 +0100

    Add mbedtls library

---
+++ HTTPS/libs/mbedtls/library/ssl_debug_helpers_generated.c
@@ -0,0 +1,39 @@
+
+
+#include "ssl_debug_helpers.h"
+
+const char *mbedtls_ssl_states_str( mbedtls_ssl_states in )
+{
+	const char* str = "mbedtls_ssl_states_str";
+	return str;
+}
+
+const char *mbedtls_ssl_protocol_version_str( mbedtls_ssl_protocol_version in )
+{
+	const char* str = "mbedtls_ssl_protocol_version_str";
+	return str;
+}
+
+const char *mbedtls_tls_prf_types_str( mbedtls_tls_prf_types in )
+{
+	const char* str = "mbedtls_tls_prf_types_str";
+	return str;
+}
+
+const char *mbedtls_ssl_key_export_type_str( mbedtls_ssl_key_export_type in )
+{
+	const char* str = "mbedtls_ssl_key_export_type_str";
+	return str;
+}
+
+const char *mbedtls_ssl_sig_alg_to_str( uint16_t in )
+{
+	const char* str = "mbedtls_ssl_sig_alg_to_str";
+	return str;
+}
+
+const char *mbedtls_ssl_named_group_to_str( uint16_t in )
+{
+	const char* str = "mbedtls_ssl_named_group_to_str";
+	return str;
+}
